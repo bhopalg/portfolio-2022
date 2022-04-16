e953431db46c8b0d17bb26368023d4d2e3484031
Gurpreet Bhopal <gbhopal91@gmail.com>
2022-04-16 20:05:35 +0100

    backgorund

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from 'src/app/app.component';
+import { BannerComponent } from 'src/app/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule
