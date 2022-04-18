4298502155c1a73e1e5920b48f96e11ef13d1434
Gurpreet Bhopal <gbhopal91@gmail.com>
2022-04-18 14:48:01 +0100

    updated borders so they only show on hover

--- src/app/contact-me/contact-me.component.ts
+++ src/app/contact-me/contact-me.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {faEnvelopeSquare, faMailBulk} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-contact-me',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactMeComponent implements OnInit {
 
+  iconEnvelopeSquare = faEnvelopeSquare;
   constructor() { }
 
   ngOnInit(): void {
