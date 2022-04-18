a583c4562b9f50032b880d46d9b59d55e3f45806
Gurpreet Bhopal <gbhopal91@gmail.com>
2022-04-18 11:11:17 +0100

    set up experiances

--- src/app/my-experience/my-experience.component.ts
+++ src/app/my-experience/my-experience.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import {faCircleCheck} from "@fortawesome/free-solid-svg-icons";
+import {IconDefinition} from "@fortawesome/fontawesome-common-types";
+
+interface Experience {
+  name: string;
+  experience: string;
+}
+
+interface Experiences {
+  [key: string]: Experience[];
+}
 
 @Component({
   selector: 'app-my-experience',
@@ -6,6 +17,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-experience.component.scss']
 })
 export class MyExperienceComponent implements OnInit {
+  iconCircleCheck = faCircleCheck
+
+  experiences: Experiences = {
+    'Frontend Development': [
+      {
+        name: 'HTML',
+        experience: 'Experienced'
+      },
+      {
+        name: 'CSS',
+        experience: 'Experienced'
+      },
+      {
+        name: 'JavaScript',
+        experience: 'Experienced'
+      },
+      {
+        name: 'Bootstrap',
+        experience: 'Experienced'
+      },
+      {
+        name: 'Tailwind',
+        experience: 'Experienced'
+      },
+      {
+        name: 'React',
+        experience: 'Experienced'
+      }
+    ],
+  }
+
+  experiencesKeys: string[] = Object.keys(this.experiences);
+
 
   constructor() { }
 
