80c9b1d6567bf3f438bc9cfd942ec84b540b0361
Gurpreet Bhopal <gbhopal91@gmail.com>
2022-04-18 12:39:21 +0100

    new experience components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FlexLayoutModule } from "@angular/flex-layout";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
 import { AppComponent } from 'src/app/app.component';
 import { BannerComponent } from 'src/app/banner/banner.component';
 import { AboutMeComponent } from 'src/app/about-me/about-me.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 import { SkillsComponent } from 'src/app/skills/skills.component';
+import { ExperiencesComponent } from 'src/app/experiences/experiences.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { SkillsComponent } from 'src/app/skills/skills.component';
     BannerComponent,
     AboutMeComponent,
     SkillsComponent,
+    ExperiencesComponent,
   ],
   imports: [
     BrowserModule,
