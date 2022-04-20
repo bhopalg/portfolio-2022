c74f5f06a04eb104cf296462482b81874d3b1bd8
Gurpreet Bhopal <gbhopal91@gmail.com>
2022-04-20 16:57:18 +0100

    skills updated

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import {faCircleCheck} from "@fortawesome/free-solid-svg-icons";
-import {IconDefinition} from "@fortawesome/fontawesome-common-types";
 
 interface Skill {
   name: string;
-  skill: string;
+  skill: 'Experienced' | 'Intermediate' | 'Beginner';
 }
 
 interface Skills {
-  [key: string]: Skill[];
+  [key: string]: {
+    [key: string]: Skill[];
+  };
 }
 
 @Component({
@@ -20,62 +21,93 @@ export class SkillsComponent implements OnInit {
   iconCircleCheck = faCircleCheck
 
   skills: Skills = {
-    'Frontend Development': [
-      {
-        name: 'HTML',
-        skill: 'Experienced'
-      },
-      {
-        name: 'CSS',
-        skill: 'Experienced'
-      },
-      {
-        name: 'JavaScript',
-        skill: 'Experienced'
-      },
-      {
-        name: 'Bootstrap',
-        skill: 'Experienced'
-      },
-      {
-        name: 'Tailwind',
-        skill: 'Experienced'
-      },
-      {
-        name: 'React',
-        skill: 'Experienced'
-      }
-    ],
-    'Backend Development': [
-      {
-        name: 'NodeJS',
-        skill: 'Experienced'
-      },
-      {
-        name: 'MongoDB',
-        skill: 'Experienced'
-      },
-      {
-        name: 'PHP',
-        skill: 'Experienced'
-      },
-      {
-        name: 'MySQL',
-        skill: 'Experienced'
-      },
-      {
-        name: 'Python',
-        skill: 'Experienced'
-      }
-    ],
+    'group1': {
+      'Frontend Development': [
+        {
+          name: 'Angular',
+          skill: 'Experienced',
+        },
+        {
+          name: 'JavaScript/Typescript',
+          skill: 'Experienced',
+        },
+        {
+          name: 'Ionic',
+          skill: 'Intermediate',
+        },
+        {
+          name: 'React',
+          skill: 'Intermediate',
+        },
+        {
+          name: 'C#',
+          skill: 'Intermediate',
+        },
+      ],
+      'Backend Development': [
+        {
+          name: 'NodeJS',
+          skill: 'Experienced',
+        },
+        {
+          name: 'MySQL',
+          skill: 'Experienced',
+        },
+        {
+          name: 'GraphQL',
+          skill: 'Experienced',
+        },
+        {
+          name: 'Solidity',
+          skill: 'Beginner',
+        },
+        {
+          name: 'GoLang',
+          skill: 'Intermediate',
+        },
+        {
+          name: 'Python',
+          skill: 'Intermediate',
+        },
+      ],
+    },
+    'group2': {
+      'Dev Ops': [
+        {
+          name: 'Docker',
+          skill: 'Experienced'
+        },
+        {
+          name: 'Elastic Container Service',
+          skill: 'Experienced',
+        },
+        {
+          name: 'Kubernetes',
+          skill: 'Beginner',
+        },
+        {
+          name: 'Ethereum Main Net',
+          skill: 'Beginner'
+        },
+        {
+          name: 'Apple App Store/Google Play Store',
+          skill: 'Intermediate',
+        },
+        {
+          name: 'Terraform',
+          skill: 'Beginner',
+        }
+      ],
+    },
   }
 
   skillKeys: string[] = Object.keys(this.skills);
-
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  getObjectKeys = (data: { [p: string]: Skill[] }) => Object.keys(data);
 
 }
