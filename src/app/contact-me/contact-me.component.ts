be90352b8b989dbfc46cc3fc018d153a81de0f05
Gurpreet Bhopal <gbhopal91@gmail.com>
2022-04-21 18:28:39 +0100

    contact me mobile

--- src/app/contact-me/contact-me.component.ts
+++ src/app/contact-me/contact-me.component.ts
@@ -1,22 +1,40 @@
-import { AfterViewInit, Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, EventEmitter, OnDestroy, OnInit, Output, ViewChild} from '@angular/core';
 import { faEnvelopeSquare } from "@fortawesome/free-solid-svg-icons";
+import {Subject, takeUntil} from "rxjs";
+import {BreakpointObserver, Breakpoints, BreakpointState} from "@angular/cdk/layout";
 
 @Component({
   selector: 'app-contact-me',
   templateUrl: './contact-me.component.html',
   styleUrls: ['./contact-me.component.scss']
 })
-export class ContactMeComponent implements AfterViewInit {
+export class ContactMeComponent implements AfterViewInit, OnInit, OnDestroy {
+  private ngUnsubscribe: Subject<boolean> = new Subject<boolean>();
 
   iconEnvelopeSquare = faEnvelopeSquare;
+  isMobile = false;
 
   @ViewChild('contactMe') contactMeRef: ElementRef | undefined;
   @Output() emitContactMeRef: EventEmitter<ElementRef> = new EventEmitter<ElementRef>();
 
-  constructor() { }
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit() {
+    this.breakpointObserver
+      .observe([Breakpoints.Small, Breakpoints.XSmall, Breakpoints.Handset])
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe((state: BreakpointState) => {
+        this.isMobile = state.matches;
+      });
+  }
 
   ngAfterViewInit() {
     this.emitContactMeRef.emit(this.contactMeRef);
+  }
+
+  ngOnDestroy(): void {
+    this.ngUnsubscribe.next(true);
+    this.ngUnsubscribe.complete();
   }
 
 }
